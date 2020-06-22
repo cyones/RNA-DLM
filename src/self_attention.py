b77312cf326fa281f294a536463b058722ce6f06
cyones <cyones77@gmail.com>
2020-06-22 04:09:20 -0300

    Fix typos

--- src/self_attention.py
+++ src/self_attention.py
@@ -2,9 +2,9 @@ import torch as tr
 import torch.nn as nn
 
 class SelfAttention(nn.Module):
-    def __init__(self, embed_dim, num_heads, dropout)
+    def __init__(self, embed_dim, num_heads, dropout):
         super(SelfAttention, self).__init__()
-        self.multihead_attention = MultiheadAttention(
+        self.multihead_attention = nn.MultiheadAttention(
             embed_dim=embed_dim,
             num_heads=num_heads,
             dropout=dropout
